385df37f906e324b990d79e11bc6861e26dc0446
Jason Meredith <signull@gmail.com>
2017-02-22 16:11:07 -0500

    fixing syntax

--- terraform/policy.tf
+++ terraform/policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "base_instance_policy" {
     name = "base_instance_policy"
-    description = "My test policy"
-    policy = ${file("policies/base-instance-policy.json")}
+    description = "update dns, set tags, pull from ecr"
+    policy = "${file("policies/base-instance-policy.json")}"
 }
