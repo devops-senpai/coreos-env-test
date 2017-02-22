699d60e08c2570153626b16197407e020ac2642d
Jason Meredith <signull@gmail.com>
2017-02-21 22:43:15 -0500

    bootstrap now more logical

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -4,6 +4,91 @@
 import requests
 import boto3
 import re
+from subprocess import call
+
+def getRole(instance):
+    for i in instance.tags:
+        if i['Key'] == "Role":
+            return i['Value']
+
+def activeRoleIps(instanceRole):
+    roleIpList = []
+    for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]):
+        for d in ec2.Instance(i.id).tags:
+            if d.get('Key', None) == 'Role':
+                if d['Value'] == instanceRole:
+                    roleIpList.append(ec2.Instance(i.id).private_ip_address)
+    return roleIpList
+
+def getZoneId(zoneName):
+    zones = dns.list_hosted_zones()
+    for zone in zones['HostedZones']:
+        if zone['Name'] == zoneName:
+            return zone['Id'].split("/")[-1]
+
+
+def roleRecords(zoneId, zoneName, roleIpList):
+    d = {}
+    for aRecord in dns.list_resource_record_sets(HostedZoneId=zoneId)['ResourceRecordSets']:
+        if aRecord['Type'] == 'A':
+            match = roleRe.match(aRecord['Name'][:-len(zoneName)-1])
+            if match:
+                d.update({ match.string: aRecord['ResourceRecords'][0]['Value'] })
+                if aRecord['ResourceRecords'][0]['Value'] not in roleIpList:
+                    d[match.string] = None
+    return d
+
+def newRecordName(sortedRecords, roleRecordsDict, zoneName, instanceRole):
+    if not sortedRecords:
+        return instanceRole + "01" + '.' + zoneName
+    for record in sortedRecords:
+        if roleRecordsDict[record] == None:
+            recordName = record + '.' + zoneName
+            return recordName
+    newRecord = instanceRole + str(int(sortedRecords[-1].lstrip(instanceRole)) + 1).zfill(2)
+    return newRecord
+
+def updateRecord(recordName, roleRecordsDict, zoneId, privateIp):
+    if recordName.rstrip('.' + zoneName) in roleRecordsDict.keys():
+        createRecord("UPSERT", zoneId, recordName, privateIp)
+    else:
+        createRecord("CREATE", zoneId, recordName, privateIp)
+
+def updateName(recordName):
+    instance.create_tags(
+            Resources = [instanceId], 
+            Tags = [
+                    {
+                        'Key': "Name", 
+                        'Value': recordName
+                    }
+                ])
+
+def setHostname(recordName):
+    retval = call(["hostnamectl", "set-hostname", recordName.rstrip('.')])
+    return retval
+
+def createRecord(action, zoneId, recordName, value ):
+    response = dns.change_resource_record_sets(
+        HostedZoneId=zoneId,
+        ChangeBatch={
+            'Changes': [
+                {
+                    'Action': action,
+                    'ResourceRecordSet': {
+                        'Name': recordName,
+                        'Type': 'A',
+                        'TTL': 60,
+                        'ResourceRecords': [
+                            {
+                                'Value': value
+                            },
+                        ],
+                    }
+                },
+            ]
+        }
+    )
 
 r = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone')
 region = r.text[:-1]
@@ -17,73 +102,30 @@ if r.status_code == 200:
 else:
     publicIp = None
 
+zoneName = "dev.example.com."
 
 ec2 = boto3.resource('ec2', region_name=region)
 
+dns = boto3.client('route53', region_name=region)
+
 instance = ec2.Instance(instanceId)
 
-for i in instance.tags:
-    if i['Key'] == "Role":
-        instanceRole = i['Value']
+instanceRole = getRole(instance)
 
-instance.create_tags(
-        Resources = [instanceId], 
-        Tags = [
-                {
-                    'Key': "Name", 
-                    'Value': "changed"
-                }
-            ])
+roleIpList = activeRoleIps(instanceRole)
 
-roleIpList = []
-for i in ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]):
-    for d in ec2.Instance(i.id).tags:
-        if d.get('Key', None) == 'Role':
-            instanceList.append({ "instanceId": i.id, "role": d['Value'] })
-            if d['Value'] == instanceRole:
-                roleIpList.append(ec2.Instance(i.id).private_ip_address)
-
-
-zoneName="dev.example.com."
-
-dns = boto3.client('route53', region_name=region)
-zones = dns.list_hosted_zones()
-for zone in zones['HostedZones']:
-    if zone['Name'] == zoneName:
-        zoneId = zone['Id'].split("/")[-1]
+zoneId = getZoneId(zoneName)
 
 roleRe = re.compile(instanceRole + '[0-9]+')
-recordList= []
-for aRecord in dns.list_resource_record_sets(HostedZoneId=zoneId)['ResourceRecordSets']:
-    if aRecord['Type'] == 'A':
-        match = roleRe.match(aRecord['Name'][:-len(zoneName)-1])
-        if match:
-            if aRecord['ResourceRecords'][0]['Value'] not in roleIpList:
-                recordName = match.string + "." + zoneName
-                createRecord(action="UPSERT", zoneId=zoneId, recordName, privateIp )
-        
 
-createRecord(action="UPSERT", zoneId=zoneId, recordName, match.string )
+roleRecordsDict = roleRecords(zoneId, zoneName, roleIpList)
+sortedRecords = sorted(roleRecordsDict.keys())
 
-def createRecord(action="UPSERT", zoneId=zoneId, name, value )
-# Actions are UPSERT, CREATE, DELETE
-    response = dns.change_resource_record_sets(
-        HostedZoneId = zoneId,
-        ChangeBatch={
-            'Changes': [
-                {
-                    'Action': action,
-                    'ResourceRecordSet': {
-                        'Name': name,
-                        'Type': 'A',
-                        'TTL': 60,
-                        'ResourceRecords': [
-                            {
-                                'Value': value
-                            },
-                            ],
-                        }
-                },
-                ]
-        }
-    )
+recordName = newRecordName(sortedRecords, roleRecordsDict, zoneName, instanceRole)
+
+updateRecord(recordName, roleRecordsDict, zoneId, privateIp)
+
+updateName(recordName)
+
+if setHostname(recordName) != 0:
+    print("ERROR: unable to set hostname")
