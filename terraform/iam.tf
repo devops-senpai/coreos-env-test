d17472ddfda4044027600e3be63dbec01f621b7c
Jason Meredith <signull@gmail.com>
2017-02-22 18:56:55 -0500

    Creating instance role

---
+++ terraform/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_instance_profile" "base_instance_role" {
+    name = "base_instance_role"
+    roles = ["${aws_iam_role.base_role.name}"]
+}
+
+resource "aws_iam_role" "base_role" {
+    name = "base_instance_role"
+    assume_role_policy = "${file("policies/role.json")}"
+}
+
+resource "aws_iam_policy" "base_policy" {
+    name = "base_instance_policy"
+    description = "view/edit tags, pull from ecr, update dns"
+    policy = "${file("policies/base-instance-policy.json")}"
+}
+
+resource "aws_iam_role_policy_attachment" "base-attach" {
+    role = "${aws_iam_role.base_role.name}"
+    policy_arn = "${aws_iam_policy.base_policy.arn}"
+}
--- terraform/policy.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_policy" "base_instance_policy" {
-    name = "base_instance_policy"
-    description = "update dns, set tags, pull from ecr"
-    policy = "${file("policies/base-instance-policy.json")}"
-}
